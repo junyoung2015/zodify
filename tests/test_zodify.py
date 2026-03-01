68c872888245f122ffa0630ade9915e88680c529
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2026-03-02 00:55:39 +0900

    fix: update version in tests to 0.3.0

--- tests/test_zodify.py
+++ tests/test_zodify.py
@@ -7,7 +7,7 @@ from zodify import validate, Optional, __version__
 # --- __version__ ---
 
 def test_version():
-    assert __version__ == "0.2.1"
+    assert __version__ == "0.3.0"
 
 
 # --- PEP 561: py.typed marker ---
